3ac98ccfdfc085490a80a2676977a639f1610fe2
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-17 00:21:35 +0300

    (2) as

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,10 +1,11 @@
 -- Write a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
 -- using an uncorrelated subquery
-select m1.origin, m1.fans
+select m1.origin as origin, m1.fans as nb_fans
 from metal_bands m1
 JOIN (
   select origin, MAX(fans) AS fans
   from metal_bands
   GROUP BY origin) as m2
-  ON m1.origin = m2.origin and m1.fans = m2.fans;
-  
+  ON m1.origin = m2.origin and m1.fans = m2.fans
+  order by m1.fans desc;
+
