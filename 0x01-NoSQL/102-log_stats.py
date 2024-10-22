27d3c1e8e12ffcd01ef2fb86bed6942d41587b25
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-22 16:16:28 +0300

    (102)

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+script that provides some stats about Nginx logs stored in MongoDB
+"""
+import pymongo
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    log_collection: pymongo.collection = client.logs.nginx
+
+    count_logs = log_collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f'{count_logs} logs')
+    print("Methods:")
+    # x = (log_collection.find())
+    # for y in x:
+    #     print(y)
+    ip_counts = {}
+    for method in methods:
+        print(f'\tmethod {method}: '
+              f'{log_collection.count_documents({"method": method})}')
+    count_status = log_collection.count_documents({
+        "method": "GET",
+        "path": '/status'
+    })
+    print(f'{count_status} status check')
+    print('IPs:')
+    pipeline = [
+        {
+            '$group': {'_id': '$ip', 'count': {"$sum": 1}}
+        },
+        {
+            '$sort': {'count': -1}
+        },
+        {
+            '$limit': 10
+        }
+    ]
+    x = log_collection.aggregate(pipeline)
+    for y in x:
+        print(y)
+
