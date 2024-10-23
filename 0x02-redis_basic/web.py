c823473726a7759fffeff0933409a1855413b0e7
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-10-23 13:36:28 +0300

    (5) different approach

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,17 +2,36 @@
 """
 web redis exercise
 """
+from functools import wraps
+from typing import Callable
+
 import requests
 import redis
 
 
+redis_instance = redis.Redis()
+
+
+def my_cache(method: Callable) -> Callable:
+    """ Caches the output of fetched data. """
+    @wraps(method)
+    def invoker(url) -> str:
+        """The wrapper function for caching the output.
+        """
+        redis_instance.incr(f'count:{url}')
+        result = redis_instance.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_instance.set(f'count:{url}', 0)
+        redis_instance.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@my_cache
 def get_page(url: str) -> str:
-    """ Obtain the HTML content of a  URL """
-    cache = redis.Redis()
-    cached_html = cache.get(f"cached:{url}")
-    if cached_html:
-        return cached_html.decode('utf-8')
-    html = requests.get(url).text
-    cache.incr("count:" + url)
-    cache.setex(f"cached:{url}", 10, html)
-    return html
+    """Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    """
+    return requests.get(url).text
