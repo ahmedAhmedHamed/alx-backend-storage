47f4ff6e5fd7437e2b816df4f40465e0466e4c95
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-23 13:51:31 +0300

    (5)

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -20,7 +20,7 @@ def cache_decorator(method: Callable) -> Callable:
         if res:
             return res.decode('utf-8')
         res = method(*args, **kwargs)
-        cache.set(str(*args), res)
+        cache.setex(str(*args), 10, res)
         return res
     return inner
 
