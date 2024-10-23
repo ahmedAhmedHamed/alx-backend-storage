af1c591e61f788f730922c560c278a1afdbe8795
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-23 13:37:38 +0300

    (5)

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+web redis exercise
+"""
+from typing import Callable
+import requests
+import redis
+
+
+def cache_decorator(method: Callable) -> Callable:
+    """
+    caches a function's result given args
+    """
+    def inner(*args, **kwargs):
+        """
+        inner func
+        """
+        cache = redis.Redis()
+        res = cache.get(str(*args))
+        if res:
+            return res.decode('utf-8')
+        res = method(*args, **kwargs)
+        cache.set(str(*args), res)
+        return res
+    return inner
+
+
+@cache_decorator
+def get_page(url: str) -> str:
+    res = requests.get(url).text
+    return res
