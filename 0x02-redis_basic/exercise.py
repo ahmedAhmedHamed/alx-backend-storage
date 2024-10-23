aacb8925e8ef7cf3bdd39955f9928ffb0d9f4d6b
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-23 12:54:15 +0300

    (1) again

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,3 +25,27 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Callable | int | str = None):
+        """
+        gets data from cache
+        """
+        if fn is None:
+            return self._redis.get(key)
+        if fn is str:
+            pass
+        if fn is int:
+            pass
+        return fn(self._redis.get(key))
+
+    def get_int(self, key: str) -> int:
+        """
+        gets key as int
+        """
+        return int(self._redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        """
+        gets key as str
+        """
+        return str(self._redis.get(key))
