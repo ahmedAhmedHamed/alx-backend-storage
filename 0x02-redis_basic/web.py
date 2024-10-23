d5820f8af6468ba2ab8de509806b935df57ff6f0
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-10-23 13:31:49 +0300

    (5)

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,41 +2,17 @@
 """
 web redis exercise
 """
-from functools import wraps
-from typing import Callable
 import requests
 import redis
 
-cache = redis.Redis()
 
-
-def count_requests(method: Callable) -> Callable:
-    """ decorator that counts requests """
-    @wraps(method)
-    def wrapper(url):
-        """ the decorator's child """
-
-        # cached_html = cache.get(f"cached:{url}")
-        # if cached_html:
-        #     return cached_html.decode('utf-8')
-        html = method(url)
-        # cache.setex(f"cached:{url}", 10, html)
-        return html
-
-    return wrapper
-
-
-@count_requests
 def get_page(url: str) -> str:
     """ Obtain the HTML content of a  URL """
+    cache = redis.Redis()
+    cached_html = cache.get(f"cached:{url}")
+    if cached_html:
+        return cached_html.decode('utf-8')
+    html = requests.get(url).text
     cache.incr("count:{" + url + "}")
-    return requests.get(url).text
-
-# url = 'http://slowwly.robertomurray.co.uk'
-# print(cache.set("count:{" + url + "}"))
-# get_page(url)
-# print(cache.get("count:{" + url + "}"))
-# get_page(url)
-# get_page(url)
-# print(cache.get("count:{" + url + "}"))
-
+    cache.setex(f"cached:{url}", 10, html)
+    return html
