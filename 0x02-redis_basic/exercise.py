0cb49d44b925b577e179328c717962921e44396f
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-23 12:52:25 +0300

    (0) nuked all later solutions temporarily

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,59 +2,9 @@
 """
 redis exercise
 """
-from functools import wraps
 import redis
 from typing import Callable, Union
 import uuid
-
-
-def replay(method: Callable):
-    """
-    method needs to be a bound function
-    """
-    print()
-    in_history = (method.__self__
-                  ._redis.lrange(method.__qualname__ + ':inputs', 0, -1))
-    out_history = (method.__self__
-                  ._redis.lrange(method.__qualname__ + ':outputs', 0, -1))
-    print(f'{method.__qualname__} was called {len(in_history)} times:')
-    for i in range(len(in_history)):
-        in_message = f'{method.__qualname__} (*{in_history[i].decode("utf-8")})'
-        arrow = ' -> '
-        out_message = out_history[i].decode("utf-8")
-        print(in_message + arrow + out_message)
-
-
-def count_calls(method: Callable) -> Callable:
-    """
-    counts the call of a particular function
-    """
-    @wraps(method)
-    def inner(self, *args, **kwargs):
-        """
-        inner doc
-        """
-        self._redis.incr(method.__qualname__)
-        return method(self, *args, **kwargs)
-
-    return inner
-
-
-def call_history(method: Callable) -> Callable:
-    """
-    sets the call history of a function
-    """
-    @wraps(method)
-    def inner(self, *args, **kwargs):
-        """
-        inner doc
-        """
-        self._redis.rpush(method.__qualname__ + ':inputs', str(args))
-        ret = method(self, *args, **kwargs)
-        self._redis.rpush(method.__qualname__ + ':outputs', ret)
-        return ret
-    return inner
-
 
 class Cache:
     """
@@ -65,10 +15,8 @@ class Cache:
         initializes the cache
         """
         self._redis = redis.Redis()
-        self._redis.flushdb(True)
+        self._redis.flushdb()
 
-    @call_history
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         stores the data in the cache
@@ -76,27 +24,3 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-
-    def get(self, key: str, fn: Callable | int | str = None):
-        """
-        gets data from cache
-        """
-        if fn is None:
-            return self._redis.get(key)
-        if fn is str:
-            pass
-        if fn is int:
-            pass
-        return fn(self._redis.get(key))
-
-    def get_int(self, key: str) -> int:
-        """
-        gets key as int
-        """
-        return int(self._redis.get(key))
-
-    def get_str(self, key: str) -> str:
-        """
-        gets key as str
-        """
-        return str(self._redis.get(key))
