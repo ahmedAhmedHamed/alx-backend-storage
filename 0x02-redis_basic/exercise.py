139dd7e6b901d6d8f79342603b5fa8867c200c0f
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-23 12:27:27 +0300

    docstrings

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,9 +8,23 @@ import redis
 import uuid
 
 
+def replay(method: Callable):
+    """
+    method needs to be a bound function
+    """
+    print(method.__qualname__)
+    print(method.__self__)
+
+
 def count_calls(method: Callable) -> Callable:
+    """
+    counts the call of a particular function
+    """
     @wraps(method)
     def inner(self, *args, **kwargs):
+        """
+        inner doc
+        """
         self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
 
@@ -18,8 +32,14 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
+    """
+    sets the call history of a function
+    """
     @wraps(method)
     def inner(self, *args, **kwargs):
+        """
+        inner doc
+        """
         self._redis.rpush(method.__qualname__ + ':inputs', str(args))
         ret = method(self, *args, **kwargs)
         self._redis.rpush(method.__qualname__ + ':outputs', ret)
@@ -41,11 +61,17 @@ class Cache:
     @call_history
     @count_calls
     def store(self, data: str | bytes | int | float) -> str:
+        """
+        stores the data in the cache
+        """
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
 
     def get(self, key: str, fn: Callable | int | str = None):
+        """
+        gets data from cache
+        """
         if fn is None:
             return self._redis.get(key)
         if fn is str:
@@ -55,7 +81,13 @@ class Cache:
         return fn(self._redis.get(key))
 
     def get_int(self, key: str) -> int:
+        """
+        gets key as int
+        """
         return int(self._redis.get(key))
 
     def get_str(self, key: str) -> str:
+        """
+        gets key as str
+        """
         return str(self._redis.get(key))
