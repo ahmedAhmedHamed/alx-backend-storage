d93dde3d7b52350e8d98c8a18956c92aabbb8a9e
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-24 23:31:31 +0300

    (5) count incrementing not an f string

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -31,7 +31,7 @@ def cache(method: Callable) -> Callable:
 @cache
 def get_page(url: str) -> str:
     """ Returns HTML content of a url """
-    count_key = f"count:{url}"
+    count_key = "count:{url}"
     redis_instance.incr(count_key)
     res = requests.get(url)
     return res.text
